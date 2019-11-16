f27d74f440316c8ef292fe94ea33aaf246b20a94
rukmrm <areyoukittenmerightmiaow@gmail.com>
2019-11-15 17:00:28 -0800

    feat: Add shelf info and Books from props

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,16 +2,21 @@ import React from 'react'
 import Book from './Book.js'
 
 class BookShelf extends React.Component {
-  state = {
-
-  }
+  state = {}
 
   render() {
     return (
-      <Book className="book"></Book>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.shelfName}</h2>
+
+        <div className="bookshelf-books">
+          {this.props.shelfBooks.map(x => {
+            return <Book className="book" bookInfo={x} />
+          })}
+        </div>
+      </div>
     )
   }
-
 }
 
 export default BookShelf
