a04fa0463b4cd5174a64194d9c806e5cff1c5339
rukmrm <areyoukittenmerightmiaow@gmail.com>
2019-11-15 16:59:23 -0800

    feat: Add book info from props

--- src/Book.js
+++ src/Book.js
@@ -1,20 +1,28 @@
 import React from 'react'
+import BookShelfChanger from './BookShelfChanger.js'
 
 class Book extends React.Component {
-  state = {
-
-  }
+  state = {}
 
   render() {
     return (
       <div className="book">
-        <div className="book-top"></div>
-        <div className="book-title"></div>
-        <div className="book-authors"></div>
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url("${this.props.bookInfo.imageLinks.thumbnail}")`,
+            }}
+          />
+          <BookShelfChanger currentShelf={this.props.bookInfo.shelf} />
+        </div>
+        <div className="book-title">{this.props.bookInfo.title}</div>
+        <div className="book-authors">{this.props.bookInfo.authors}</div>
       </div>
     )
   }
-
 }
 
 export default Book
