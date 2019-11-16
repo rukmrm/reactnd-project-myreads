4a64878e87acac92ba1f7c1742a188dd85722b07
rukmrm <areyoukittenmerightmiaow@gmail.com>
2019-11-15 16:57:54 -0800

    style: formatting

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 class BookShelfChanger extends React.Component {
-  state = {
-
-  }
+  state = {}
 
   render() {
     return (
       <div className="book-shelf-changer">
         <select>
-          <option value="move" disabled>Move to...</option>
+          <option value="move" disabled>
+            Move to...
+          </option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
@@ -18,7 +18,6 @@ class BookShelfChanger extends React.Component {
       </div>
     )
   }
-
 }
 
 export default BookShelfChanger
