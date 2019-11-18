7c72834e6ff82dbd3f285a4b030d06b01781d68f
rukmrm <areyoukittenmerightmiaow@gmail.com>
2019-11-18 10:21:31 -0800

    feat: Test for book properties before adding elements

--- src/Book.js
+++ src/Book.js
@@ -8,23 +8,37 @@ class Book extends React.Component {
     return (
       <li key={this.props.bookInfo.id}>
         <div className="book">
-          <div className="book-top">
-            <div
-              className="book-cover"
-              style={{
-                width: 128,
-                height: 193,
-                backgroundImage: `url("${this.props.bookInfo.imageLinks.thumbnail}")`,
-              }}
-            />
-            <BookShelfChanger
-              currentShelf={this.props.bookInfo.shelf}
-              handleBookShelfChange={this.props.handleBookShelfChange}
-              bookObj={this.props.bookInfo}
-            />
-          </div>
-          <div className="book-title">{this.props.bookInfo.title}</div>
-          <div className="book-authors">{this.props.bookInfo.authors.join(', ')}</div>
+          {this.props.bookInfo.imageLinks && this.props.bookInfo.imageLinks.thumbnail ? (
+            <div className="book-top">
+              <div
+                className="book-cover"
+                style={{
+                  width: 128,
+                  height: 193,
+                  backgroundImage: `url("${this.props.bookInfo.imageLinks.thumbnail}")`,
+                }}
+              />
+              <BookShelfChanger
+                currentShelf={this.props.bookInfo.shelf}
+                handleBookShelfChange={this.props.handleBookShelfChange}
+                bookObj={this.props.bookInfo}
+              />
+            </div>
+          ) : (
+            ''
+          )}
+
+          {this.props.bookInfo.title ? (
+            <div className="book-title">{this.props.bookInfo.title}</div>
+          ) : (
+            ''
+          )}
+
+          {this.props.bookInfo.authors ? (
+            <div className="book-authors">{this.props.bookInfo.authors.join(', ')}</div>
+          ) : (
+            ''
+          )}
         </div>
       </li>
     )
